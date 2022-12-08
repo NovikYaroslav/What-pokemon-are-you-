5dd9e2573ff6fa2314464dc024d567985b75efa3
Yaroslav Novik <yaroslavnowik@yandex.ru>
2022-12-08 18:10:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const button = document.querySelector(".pokebutton");
+const result = document.querySelector(".result")
+const image = document.querySelector(".sprite")
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function showMyPokemon() {
+ const randomPokemon =  getRandomInt(10)
+ if (randomPokemon == 3) {
+    result.textContent = `You NOT Pokemon!`
+    image.classList.remove("sprite_visible")
+ } 
+ else {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.sprites.front_shiny)
+        const pokemonName = data.name
+        const UpperPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
+        result.textContent = `You ${UpperPokemonName}`
+        image.src = data.sprites.front_shiny
+        image.classList.add("sprite_visible")
+
+    })
+ }
+
+}
+
+button.addEventListener("click", showMyPokemon);
