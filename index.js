1f7c1c67afe866ad8f74618b974ba7cd2d64496d
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-06-02 15:43:04 +0300

    start of refactoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const button = document.querySelector(".pokebutton");
-const result = document.querySelector(".result");
-const image = document.querySelector(".sprite");
+const button = document.querySelector('.pokebutton');
+const result = document.querySelector('.result');
+const image = document.querySelector('.sprite');
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -11,20 +11,19 @@ function showMyPokemon() {
   const hasbik = getRandomInt(100);
   if (hasbik <= 2) {
     result.textContent = `You lucky! You found Hasbik!`;
-    image.src = "./hasbulla-hasbi.gif";
+    image.src = './hasbulla-hasbi.gif';
   } else {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data.sprites.front_shiny);
         const pokemonName = data.name;
-        const UpperPokemonName =
-          pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+        const UpperPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
         result.textContent = `You found ${UpperPokemonName}`;
         image.src = data.sprites.front_shiny;
-        image.classList.add("sprite_visible");
+        image.classList.add('sprite_visible');
       });
   }
 }
 
-button.addEventListener("click", showMyPokemon);
+button.addEventListener('click', showMyPokemon);
